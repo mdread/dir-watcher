fc435a25ec57f26f81579d7ba843d704049cac2f
Daniel Camarda <0xcaos@gmail.com>
2012-11-14 14:56:38 +0100

    added assembly plugin to sbt

--- project/DirwatcherBuild.scala
+++ project/DirwatcherBuild.scala
@@ -1,12 +1,14 @@
 import sbt._
 import sbt.Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
 
 object DirwatcherBuild extends Build {
 
   lazy val dirwatcher = Project(
     id = "dir-watcher",
     base = file("."),
-    settings = Project.defaultSettings ++ Seq(
+    settings = Project.defaultSettings ++ assemblySettings ++ Seq(
       name := "dir-watcher",
       organization := "net.caoticode.dirwatcher",
       version := "0.1-SNAPSHOT",
