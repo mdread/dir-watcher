93a2f7c54d06dbe581ed2293126c7ee551790dfc
Daniel Camarda <maniacal.dread@gmail.com>
2015-07-30 19:26:53 +0200

    scala/akka/assembly-plugin versions updated

--- project/DirwatcherBuild.scala
+++ project/DirwatcherBuild.scala
@@ -1,22 +1,17 @@
 import sbt._
 import sbt.Keys._
-import sbtassembly.Plugin._
-import AssemblyKeys._
 
 object DirwatcherBuild extends Build {
 
   lazy val dirwatcher = Project(
     id = "dir-watcher",
     base = file("."),
-    settings = Project.defaultSettings ++ assemblySettings ++ Seq(
+    settings = Project.defaultSettings ++ Seq(
       name := "dir-watcher",
       organization := "net.caoticode.dirwatcher",
       version := "0.1-SNAPSHOT",
-      scalaVersion := "2.9.2",
-      resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
-      resolvers += "Twitter Repository" at "http://maven.twttr.com/",
-      libraryDependencies += "com.typesafe.akka" % "akka-actor" % "2.0.3",
-      libraryDependencies += "com.twitter"   % "util-eval"   % "5.3.13"
+      scalaVersion := "2.11.7",
+      libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.12"
     )
   )
 }
